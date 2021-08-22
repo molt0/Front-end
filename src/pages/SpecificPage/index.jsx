c8c5fa5f245759bc98d7ea166b122f78e0d9069e
HARDTACK-Dev <rjsdn827@gmail.com>
2021-08-22 21:10:19 +0900

    chore: TOAST Editor 삭제

--- src/pages/SpecificPage/index.jsx
+++ src/pages/SpecificPage/index.jsx
@@ -1,23 +1,40 @@
 import React from "react";
 import styled from "styled-components";
-import Header from "../../components/SpecificPage/Header"
-import HeadInfo from "../../components/SpecificPage/HeadInfo"
-import ButtonMenus from "../../components/SpecificPage/ButtonMenus"
-import TextEditor from "../../components/SpecificPage/TextEditor"
+import Header from "../../components/SpecificPage/Header";
+import HeadInfo from "../../components/SpecificPage/HeadInfo";
+import ButtonMenus from "../../components/SpecificPage/ButtonMenus";
 
-import { Divider } from "@chakra-ui/react"
+
+import { Divider, Switch, FormControl, FormLabel } from "@chakra-ui/react";
+
+const Field = styled.div`
+  width: 90%;
+  height: 90%;
+
+  margin: 0 auto;
+  margin-top: 10px;
+`;
 
 const SpecificPage = () => {
-  return(
+  return (
     <div>
       <Header />
       <HeadInfo />
       <ButtonMenus />
-      <Divider mt="30px" colorScheme="whiteAlpha"/>
-      <TextEditor />
+
+      <FormControl display="flex" alignItems="center">
+        <FormLabel htmlFor="email-alerts" mb="0">
+          미리보기 모드
+        </FormLabel>
+        <Switch size="lg" id="email-alerts" />
+      </FormControl>
+
+      <Divider mt="30px" colorScheme="whiteAlpha" />
+      <Field>
+        
+      </Field>
     </div>
   );
-}
+};
 
 export default SpecificPage;
-
