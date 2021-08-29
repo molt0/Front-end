e570888ceab98cef6642abd9c4f43fba30bce4ee
jd053303 <jd053303@gmail.com>
2021-08-30 06:03:52 +0900

    fix: Box 크기 수정

--- src/components/MainPage/RecentSearch.jsx
+++ src/components/MainPage/RecentSearch.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import styled from "styled-components";
 import {
-  Link,
   Box,
   Heading, 
   Button,
@@ -9,17 +8,14 @@ import {
 } from "@chakra-ui/react";
 
 
-
 const RecentSearch = () => {
   return(
-    <Box mt="30px">
-      <Heading size="md" color="gray.500">최근 목록</Heading>
-      <Box width="1500px" height="200px" maxW="1500px" mx="auto" mt="10px" borderRadius="md" boxShadow="lg" bg="#fbfbfb;">
-        <Link href="login" _hover={{ textDecoration: 'none' }}>
-          <Button onclick="locantion.href='login'" variant="outline" width="200px" height="50px" left="44%" top="35%" borderRadius="lg" bg="teal.300" color="#fff" fontSize="xl" fontWeight="extrabold">
-            로그인
-          </Button>
-        </Link>
+    <Box mt="100px">
+      <Heading size="sm" color="gray.500">최근 목록</Heading>
+      <Box width="1100px" height="150px" maxW="1100px" mx="auto" mt="10px" borderRadius="md" boxShadow="lg" bg="#fbfbfb;">
+        <Button className="btn" variant="outline" width="140px" height="37px" left="44%" top="35%" borderRadius="lg" bg="teal.300" color="#fff" fontSize="sm" fontWeight="extrabold">
+          로그인
+        </Button>
       </Box>
     </Box>
   );
