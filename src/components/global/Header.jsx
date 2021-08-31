bd9628e949a40778a8d2092ae2f45b28e3227c0b
jd053303 <jd053303@gmail.com>
2021-08-31 11:51:49 +0900

    feat: 세부메뉴 추가
    map 지정 수정 해양 ㅇ마놀해;ㅁㅈㄴ

--- src/components/global/Header.jsx
+++ src/components/global/Header.jsx
@@ -10,7 +10,8 @@ import {
   Link,
   Image,
   SimpleGrid,
-  Center
+  Center,
+  Collapse
 } from "@chakra-ui/react";
 import {
   Drawer,
@@ -20,48 +21,187 @@ import {
   DrawerContent,
   DrawerCloseButton,
 } from "@chakra-ui/react";
-import { HamburgerIcon } from "@chakra-ui/icons";
+import { HamburgerIcon, ChevronDownIcon, ChevronUpIcon } from "@chakra-ui/icons";
 
 const Header = (props) => {
-    const btnRef = React.useRef()
-    const {isOpen, onOpen, onClose } = useDisclosure()
-    const [size, setSize] = React.useState("xs")
-    const changeStatus = () => setSize(size === 'xs' ? 'lg' : 'xs');
+  const btnRef = React.useRef()
+  const { isOpen, onOpen, onClose } = useDisclosure()
+  const [size, setSize] = React.useState("xs")
+  const changeStatus = () => setSize(size === 'xs' ? 'lg' : 'xs');
+  const [show, setShow] = React.useState(false)
+  const handleToggle = () => setShow(!show)
 
-
-    const NavList = [
-      {
-        key : 1,
-        genre : "발라드"
-      },
-      {
-        key : 2,
-        genre : "댄스"
-      },
-      {
-        key : 3,
-        genre : "POP"
-      },
-      {
-        key : 4,
-        genre : "R & B / Soul"
-      },
-      {
-        key : 5,
-        genre : "록 / 메탈"
-      }
-    ]
+  const genre = [
+    {
+      key: 1,
+      name: "K-POP"
+    },
+    {
+      key: 2,
+      name: "POP"
+    },
+    {
+      key: 3,
+      name: "ROCK"
+    },
+    {
+      key: 4,
+      name: "ELEC"
+    },
+    {
+      key: 5,
+      name: "RAP"
+    },
+    {
+      key: 6,
+      name: "RNB"
+    },
+    {
+      key: 7,
+      name: "FOLK"
+    },
+  ]
+  const KPOP = [
+    {
+      key: 1,
+      genre: "K-POP"
+    },
+    {
+      key: 2,
+      genre: "발라드"
+    },
+    {
+      key: 3,
+      genre: "댄스"
+    }
+  ]
+  const POP = [
+    {
+      key: 1,
+      genre: "POP"
+    },
+    {
+      key: 2,
+      genre: "얼터너티브팝"
+    },
+    {
+      key: 3,
+      genre: "올디스"
+    }
+  ]
+  const ROCK = [
+    {
+      key: 1,
+      genre: "록 / 메탈"
+    },
+    {
+      key: 2,
+      genre: "모던록"
+    },
+    {
+      key: 3,
+      genre: "얼터너티브록"
+    },
+    {
+      key: 4,
+      genre: "프로그레시브/아트록"
+    },
+    {
+      key: 5,
+      genre: "하드록"
+    },
+    {
+      key: 6,
+      genre: "헤비메탈"
+    },
+    {
+      key: 7,
+      genre: "뉴메탈/하드코어"
+    },
+    {
+      key: 8,
+      genre: "포스트록"
+    }
+  ]
+  const ELEC = [
+    {
+      key: 1,
+      genre: "일렉트로니카"
+    },
+    {
+      key: 2,
+      genre: "하우스"
+    },
+    {
+      key: 3,
+      genre: "클럽뮤직"
+    }
+  ]
+  const RAP = [
+    {
+      key: 1,
+      genre: "랩 / 힙합"
+    },
+    {
+      key: 2,
+      genre: "팝랩"
+    },
+    {
+      key: 3,
+      genre: "얼터너티브힙합"
+    },
+    {
+      key: 4,
+      genre: "갱스터/하드코어랩"
+    },
+    {
+      key: 6,
+      genre: "East&West"
+    }
+  ]
+  const RNB = [
+    {
+      key: 1,
+      genre: "R&B / SOUL"
+    },
+    {
+      key: 2,
+      genre: "R&B"
+    },
+    {
+      key: 3,
+      genre: "SOUL"
+    },
+    {
+      key: 4,
+      genre: "URBAN"
+    }
+  ]
+  const FOLK = [
+    {
+      key: 1,
+      genre: "포크"
+    },
+    {
+      key: 2,
+      genre: "블루스"
+    },
+    {
+      key: 3,
+      genre: "컨트리"
+    }
+  ]
   return (
     <Flex
-      pos= "fixed"
+      pos="fixed"
       padding="11.5px"
       w="100%"
-      bg= {props.bgColor}
-      backdropFilter= {props.bgblur}  
-      t= "0" l= "0" r= "0"
+      bg={props.bgColor}
+      backdropFilter={props.bgblur}
+      t="0" l="0" r="0"
       color="white"
       align="center"
-      transition= "all ease 1s 0s"
+      transition="all ease 1s 0s"
       {...props}
     >
       <Flex align="center" mr={5}>
@@ -69,12 +209,12 @@ const Header = (props) => {
           Molto
         </Heading>
       </Flex>
-      
+
       <Spacer />
       <Stack w="500px" mr="10" spacing={3}>
         <Input ml="60px" h="35px" w="430px" borderRadius="7px" variant="outline" color="black" fontSize="14px" bg="white" placeholder="검색" />
       </Stack>
-      
+
       <Button position="fix" size="sm" mr="40px" ref={btnRef} colorScheme="teal" onClick={onOpen}>
         <HamburgerIcon />
       </Button>
@@ -89,50 +229,59 @@ const Header = (props) => {
         <DrawerOverlay />
         <DrawerContent>
           <DrawerCloseButton />
-            {
-              size === "lg" ?
-              (                 
+          {
+            size === "lg" ?
+              (
                 <DrawerFooter>
-                    <Button variant="outline" m="30px" onClick={() => changeStatus()}>
-                      Cancel
+                  <Button variant="outline" m="30px" onClick={() => changeStatus()}>
+                    Cancel
                     </Button>
-                    <Button colorScheme="blue" onClick={() => changeStatus()}>Save</Button>
+                  <Button colorScheme="blue" onClick={() => changeStatus()}>Save</Button>
                 </DrawerFooter>
               ) :
               (
                 <DrawerBody>
-                <Image
-                  boxSize="2rem"
-                  borderRadius="full"
-                  src="https://placekitten.com/100/100"
-                  alt="Fluffybuns the destroyer"
-                  m="12px"
-                />
-                <Button variant="outline" onClick={() => changeStatus()}>
+                  <Image
+                    boxSize="2rem"
+                    borderRadius="full"
+                    src="https://placekitten.com/100/100"
+                    alt="Fluffybuns the destroyer"
+                    m="12px"
+                  />
+                  <Button variant="outline" onClick={() => changeStatus()}>
                     프로필 수정
-                </Button>
-                {
-                  <Collapse startingHeight={100} in={show}>
-                  <SimpleGrid columns={1} spacingX="100px">
+                  </Button>
                   {
-                    NavList.map(Items => (
+                    <SimpleGrid columns={1} spacingX="100px">
                       <Center>
-                        <SimpleGrid columns={1} width="100%" height="60px" textAlign="center" mt="10px" ml="10px" lineHeight="60px" fontSize="20px" fontWeight="extrabold" borderRadius="lg" boxShadow="lg" bg="#fbfbfb;">
-                        <Link color="teal.500" href="/editor">
-                          {Items.genre}
-                        </Link>  
-                      </SimpleGrid>
+                        <SimpleGrid columns={1} width="100%" textAlign="center" mt="10px" ml="10px" lineHeight="60px" fontSize="20px" fontWeight="extrabold" borderRadius="lg" boxShadow="lg" bg="#fbfbfb;">
+                          {
+                            genre.map(menu => (
+                              <Collapse startingHeight={50} in={show}>
+                              {
+                                [menu.name].map(Items => (
+                                  console.log(menu.name),
+                                  <Flex>
+                                    <Link color="teal.500" width="100%" height="60px" w="100%" href="/editor">
+                                      {Items.genre}
+                                    </Link>
+                                    <Button size="sm" onClick={handleToggle} mt="1rem">
+                                      {show ? <ChevronDownIcon /> : <ChevronUpIcon />}
+                                    </Button>
+                                  </Flex>
+                                ))
+                              }
+                              </Collapse>
+                            ))
+                          }
+                        </SimpleGrid>
                       </Center>
-                  ))
-                }
-                  </SimpleGrid>
-                </Collapse>
-                }
-                
+                    </SimpleGrid>
+                  }
                 </DrawerBody>
               )
-            }
-          </DrawerContent>
+          }
+        </DrawerContent>
       </Drawer>
     </Flex>
   );
