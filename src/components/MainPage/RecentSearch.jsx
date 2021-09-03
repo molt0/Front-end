81eda3636ec9e2a84a26c45fdbeb13e3ce627f50
jd053303 <jd053303@gmail.com>
2021-09-03 11:32:38 +0900

    fix: Link LoginPage

--- src/components/MainPage/RecentSearch.jsx
+++ src/components/MainPage/RecentSearch.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Box, Heading, Button, Container } from "@chakra-ui/react";
+import { Box, Heading, Button, Container, Link } from "@chakra-ui/react";
 
 
 const RecentSearch = () => {
@@ -7,9 +7,11 @@ const RecentSearch = () => {
     <Box mt="100px">
       <Heading size="sm" color="gray.500">최근 목록</Heading>
       <Box width="1100px" height="150px" maxW="1100px" mx="auto" mt="10px" borderRadius="md" boxShadow="lg" bg="#fbfbfb;">
-        <Button className="btn" variant="outline" width="140px" height="37px" left="44%" top="35%" borderRadius="lg" bg="teal.300" color="#fff" fontSize="sm" fontWeight="extrabold">
-          로그인
-        </Button>
+        <Link href="/login">
+          <Button className="btn" variant="outline" width="140px" height="37px" left="44%" top="35%" borderRadius="lg" bg="teal.300" color="#fff" fontSize="sm" fontWeight="extrabold">
+            로그인
+          </Button>
+        </Link>
       </Box>
     </Box>
   );
