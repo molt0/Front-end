538e628512b6998304300a6481bc9d70f87ff3f1
hansaekim <75602585+hansaekim@users.noreply.github.com>
2021-09-07 00:55:13 +0900

    feat: DrawerContent 프로필 수정 생성

--- src/components/global/Header.jsx
+++ src/components/global/Header.jsx
@@ -11,6 +11,9 @@ import {
   Image,
   SimpleGrid,
   Collapse,
+  Box,
+  IconButton,
+  AddIcon
 } from "@chakra-ui/react";
 import {
   Drawer,
@@ -22,6 +25,13 @@ import {
 } from "@chakra-ui/react";
 
 import { HamburgerIcon, ChevronDownIcon, ChevronUpIcon } from "@chakra-ui/icons";
+
+import IdInput from "../../components/UserPage/IdInput";
+import PsInput from "../../components/UserPage/PsInput";
+import PsChInput from "../../components/UserPage/PsChInput";
+import PsChInput2 from "../../components/UserPage/PsChInput2";
+import NameInput from "../../components/UserPage/NameInput";
+import EmailInput from "../../components/UserPage/EmailInput";
 
 const Header = (props) => {
   const btnRef = React.useRef()
@@ -185,10 +195,40 @@ const Header = (props) => {
             size === "lg" ?
               (
                 <DrawerFooter>
-                  <Button variant="outline" m="30px" onClick={() => changeStatus()}>
-                    Cancel
-                    </Button>
-                  <Button colorScheme="blue" onClick={() => changeStatus()}>Save</Button>
+                  <Box width="600px" height="900px">
+                  <Heading width="600px" pl="220px" mt="200px">프로필 수정</Heading>
+                    <Box width="150px" height="600px" float="left">
+                      <Image
+                        boxSize="125px"
+                        borderRadius="full"
+                        src="https://placekitten.com/100/100"
+                        alt="Fluffybuns the destroyer"
+                        mt="150px"
+                      />
+                      <Button width="50px" height="50px" top="-40px" left="85px" bg="teal.300" color="#fff" borderRadius="25px" pb="10px" fontSize="xxx-large">+</Button>
+                    </Box>
+                    <Box width="350px" height="600px" float="left">
+                      
+                      <IdInput />
+                      <PsChInput />
+                      <PsChInput2 />
+                      <NameInput />
+                      <Button variant="outline" width="50px" height="50px" left="350px" mb="0" mt="-10px" borderRadius="lg" bg="teal.300" color="#fff" fontSize="xl" fontWeight="extrabold">
+                        수정
+                      </Button>
+                      <EmailInput />
+                      <Button variant="outline" width="50px" height="50px" left="350px" mb="0" mt="-60px" borderRadius="lg" bg="teal.300" color="#fff" fontSize="xl" fontWeight="extrabold">
+                        수정
+                      </Button>
+                      <PsInput />
+                      <Button variant="outline" width="250px" height="50px" left="50px" top="50px" borderRadius="lg" bg="teal.300" color="#fff" fontSize="xl" fontWeight="extrabold" onClick={() => changeStatus()}>
+                        정보변경
+                      </Button>
+                      <Button  variant="outline" width="100px" height="50px" left="50px" top="50px" borderRadius="lg" bg="gray" color="#fff" fontSize="xl" fontWeight="extrabold" onClick={() => changeStatus()}>
+                        취소
+                      </Button>
+                    </Box>
+                  </Box>
                 </DrawerFooter>
               ) :
               (
