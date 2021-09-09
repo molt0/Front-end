749b771be3b01653f0451bdfbe814deed00bf57d
dks2312 <59419591+dks2312@users.noreply.github.com>
2021-09-09 16:43:58 +0900

    chore: 앨범 크기 작게 수정

--- src/components/SearchPage/Album.jsx
+++ src/components/SearchPage/Album.jsx
@@ -1,8 +1,7 @@
 import { 
-  background,
-  Box, 
-  Center, 
-  Flex, scaleFadeConfig, Spacer  
+  Box,  
+  Flex, 
+  Image
 } from '@chakra-ui/react';
 import React, { useState } from 'react';
 
@@ -13,19 +12,19 @@ const Album = ({album, isVertHoriz="horizontal"}) =>{
     borderRadius:"10px", 
     background:'var(--chakra-colors-teal-100)',
     transform: "scale(1.05, 1.05) ",
-    transition:"transform 0.1s"
+    transition:"all 0.1s"
   }
   const vertHoverStyle = {
     background:"linear-gradient(to left, #fdfdfd 0%, var(--chakra-colors-teal-100) 20%, var(--chakra-colors-teal-100) 80%, #fdfdfd 100%)",
     transform: "scale(1.05, 1.05) ",
-    transition:"transform 0.1s"
+    transition:"all 0.1s"
   }
 
   return(
     isVertHoriz == "horizontal" ? 
     (
       <Flex 
-        h={250} pt="5px"
+        h={200} pt="5px"
         direction='column' 
         alignItems='center' 
         onMouseEnter={() => {setIsHoverAlbum(true)}} 
@@ -33,7 +32,7 @@ const Album = ({album, isVertHoriz="horizontal"}) =>{
         style={isHoverAlbum ? horizHoverStyle : {transition:"transform 0.2s"}}
         overflow="hidden"
       >
-        <Box flex="10"><img src={album.img} alt={album.name} /></Box>
+        <Box flex="10"><Image src={album.img} alt={album.name} fallbackSrc="https://via.placeholder.com/150" fit="cover" boxSize="100px"/></Box>
         <Box flex="2">{album.name}</Box>
         <Box flex="2">{album.artist}</Box>
         <Box flex="1">{ Math.floor(album.time/60) + ":"+ album.time%60 }</Box>
@@ -41,15 +40,16 @@ const Album = ({album, isVertHoriz="horizontal"}) =>{
     ) :
     (
       <Flex 
-        h={170} pl="50px" pr="50px"              
+        h={100} pl="50px" pr="50px"              
         direction='row' 
         alignItems='center' 
         onMouseEnter={() => {setIsHoverAlbum(true)}} 
         onMouseLeave={() => {setIsHoverAlbum(false)}}
-        style={isHoverAlbum ? vertHoverStyle : {transition:"transform 0.2s"}}
+        style={isHoverAlbum ? vertHoverStyle : { transition:"transform 0.2s"}}
         overflow="hidden"
+        borderBottom="1px solid var(--chakra-colors-teal-300)"
       >
-        <Box flex="3"><img src={album.img} alt={album.name} /></Box>
+        <Box flex="3"><Image src={album.img} alt={album.name} fallbackSrc="https://via.placeholder.com/150" fit="cover" boxSize="80px"/></Box>
         <Box flex="9" pl="10px">{album.name}</Box>
         <Box flex="2">{album.artist}</Box>
         <Box flex="1">{ Math.floor(album.time/60) + ":"+ album.time%60 }</Box>
