481ff32c3c2502b27169fa03da33ece8eaa6032a
Gunwoo Ha <rjsdn827@gmail.com>
2021-09-10 11:39:03 +0900

    Update: 404페이지

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -1,13 +1,35 @@
 import React from 'react';
 import { Button, Link } from '@chakra-ui/react'
+import styled from 'styled-components';
+
+const Background = styled.div`
+    width: 100vw;
+    height: 100vh;
+
+    margin: 0 auto;
+
+    background-color: #91e9c4;
+`;
+const MainTitle = styled.p`
+    font-size: 70px;
+`;
+
+const Center = styled.div`
+    width: 1200px;
+    margin: 0 auto;
+`
 
 const NotFoundPage = () =>{
     return(
         <>
-            <h1>페이지를 찾을 수 없습니다!</h1>
-            <Link href="./">
-                <Button>홈페이지로 돌아가기</Button>
-            </Link>
+            <Background>
+                <Center>
+                    <MainTitle>404 NOT FOUND!</MainTitle>
+                    <Link href="./">
+                        <Button>홈페이지로 돌아가기</Button>
+                    </Link>
+                </Center>
+            </Background>
         </>
     )
 }
