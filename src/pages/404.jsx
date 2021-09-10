119caa529b86842f22b04b3395799174340fabd5
Gunwoo Ha <rjsdn827@gmail.com>
2021-09-10 10:56:28 +0900

    update: 홈페이지 이동

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -5,7 +5,7 @@ const NotFoundPage = () =>{
     return(
         <>
             <h1>페이지를 찾을 수 없습니다!</h1>
-            <Link href="/">
+            <Link href="./">
                 <Button>홈페이지로 돌아가기</Button>
             </Link>
         </>
