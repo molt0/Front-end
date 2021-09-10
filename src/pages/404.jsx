2a5305967f8cf388dac3e252ae8e37243f08142f
Gunwoo Ha <rjsdn827@gmail.com>
2021-09-10 10:52:51 +0900

    feat: 404페이지 추가

---
+++ src/pages/404.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Button, Link } from '@chakra-ui/react'
+
+const NotFoundPage = () =>{
+    return(
+        <>
+            <h1>페이지를 찾을 수 없습니다!</h1>
+            <Link href="/">
+                <Button>홈페이지로 돌아가기</Button>
+            </Link>
+        </>
+    )
+}
+
+export default NotFoundPage;
