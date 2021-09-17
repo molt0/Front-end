180a5b8202e7bd4bce1c228ee5d8d68a870d36ac
jd053303 <jd053303@gmail.com>
2021-09-17 13:47:08 +0900

    fix: LoginPage 디자인 변경

--- src/pages/LoginPage/index.jsx
+++ src/pages/LoginPage/index.jsx
@@ -1,15 +1,15 @@
 import React from "react";
 import LoginForm from "../../components/LoginPage/LoginForm";
-import { Container, Flex, Box, Heading, Button } from "@chakra-ui/react";
+import { Container, Flex, Box, Link, Text } from "@chakra-ui/react";
 
 const LoginPage = () => {
   return (
     <Container maxW="1500px" mx="auto">
       <Flex mt="100px">
         <Box width="500px" height="700px" maxW="1500px" mx="auto" mt="30px" borderRadius="md" boxShadow="lg" bg="#fbfbfb;">
-          <Heading width="150px" ml="200px" mt="50px">로그인</Heading>
           <LoginForm />
-        </Box>  
+          <Text fontSize="sm" ml="110px" mt="100px">이곳이 처음이라면 <Link color="blue">여기</Link>를 눌러주세요.</Text>
+        </Box>
       </Flex>
     </Container>
   );
