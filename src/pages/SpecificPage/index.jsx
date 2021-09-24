7a1332da56a37b522a401a1447be27b5fadaff36
Gunwoo Ha <rjsdn827@gmail.com>
2021-09-24 13:24:01 +0900

    feat: 세부정보 페이지 헤더 스크롤 기능 구현

--- src/pages/SpecificPage/index.jsx
+++ src/pages/SpecificPage/index.jsx
@@ -1,11 +1,13 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import styled from "styled-components";
-import SHeader from "../../components/SpecificPage/Header";
+import PageHeader from "../../components/SpecificPage/Header";
 import HeadInfo from "../../components/SpecificPage/HeadInfo";
 import ButtonMenus from "../../components/SpecificPage/ButtonMenus";
 import ContentViewer from "../../components/SpecificPage/ContentViewer";
 
-import Background from "../../components/global/Background";
+import Header from "../../components/global/Header"
+
+// import Background from "../../components/global/Background";
 
 import { Divider, Switch, FormControl, FormLabel } from "@chakra-ui/react";
 
@@ -21,12 +23,43 @@ const ContentContainer = styled.div`
   border-radius: 20px;
 `;
 
+const Spacer = styled.div`
+  width: 100%;
+  height: 70px;
+`;
+
+
+const HideWhenScroll = styled.div`
+  width: 100%;
+
+  opacity: ${props => props.visibilty ? 1 : 0};
+  transition: all 0.3s ease-in-out;
+`;
+
 const SpecificPage = () => {
+
+  const [scrollPosition, setScrollPosition] = useState(0);
+  const [footerVisible, setVisible] = useState(false);
+
+  const updateScroll = () => {
+    setScrollPosition(window.scrollY || document.documentElement.scrollTop);
+  }
+
+  useEffect(()=>{
+    window.addEventListener('scroll', updateScroll);
+    setVisible(!(scrollPosition > 15))
+    console.log(footerVisible)
+  });
+
   return (
     <div>
-      
+      <HideWhenScroll visibilty={footerVisible}>
+        <Header />
+      </HideWhenScroll>
+      <Spacer />
       <ContentContainer>
-        <SHeader />
+        <PageHeader />
+
         
         <HeadInfo />
         <ButtonMenus />
