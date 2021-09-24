27c8fe59891919c7a5cff4f337c2a0d1d3cbf41c
Gunwoo Ha <rjsdn827@gmail.com>
2021-09-24 10:14:27 +0900

    feat: 카테고리별 버튼 Toggle 구현

--- src/components/SpecificPage/ButtonMenus.jsx
+++ src/components/SpecificPage/ButtonMenus.jsx
@@ -1,26 +1,53 @@
-import React from "react";
-import styled from "styled-components";
+import React, { useState } from "react";
 import { Button, Flex } from "@chakra-ui/react";
 
 const Header = () => {
+  const [toggles, setToggles] = useState([
+    { isToggle: true, text: "곡 소개" },
+    { isToggle: false, text: "가사" },
+    { isToggle: false, text: "정보" },
+    { isToggle: false, text: "기타" },
+    { isToggle: false, text: "관련 미디어" },
+  ]);
+
+  //i is for index
+  const onClick = (index) => {
+    setToggles((prevState) =>
+      toggles.map((x, mIndex) =>
+        mIndex === index
+          ? { isToggle: true, text: x.text }
+          : { isToggle: false, text: x.text }
+      )
+    );
+  };
+
   return (
     <div>
       <Flex mt="30px" ml="50px">
-        <Button w="200px" colorScheme="teal" size="md" >
-          곡 소개
-        </Button>
-        <Button w="200px" colorScheme="teal" size="md" variant="outline"  ml="10px">
-          가사
-        </Button>
-        <Button w="200px" colorScheme="teal" size="md" variant="outline"  ml="10px">
-          정보
-        </Button>
-        <Button w="200px" colorScheme="teal" size="md" variant="outline"  ml="10px">
-          기타
-        </Button>
-        <Button w="200px" colorScheme="teal" size="md" variant="outline"  ml="10px">
-          관련 미디어
-        </Button>
+        {toggles.map((toggle, i) =>
+          toggle.isToggle ? (
+            <Button
+              w="200px"
+              colorScheme="teal"
+              size="md"
+              ml="10px"
+              onClick={() => onClick(i)}
+            >
+              {toggle.text}
+            </Button>
+          ) : (
+            <Button
+              w="200px"
+              colorScheme="teal"
+              size="md"
+              variant="outline"
+              ml="10px"
+              onClick={() => onClick(i)}
+            >
+              {toggle.text}
+            </Button>
+          )
+        )}
       </Flex>
     </div>
   );
