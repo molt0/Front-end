41ea005422ed731315d65734c06f89073ca261f4
jd053303 <jd053303@gmail.com>
2021-09-29 16:25:35 +0900

    feat: MainIntro 추가

---
+++ src/components/MainPage/intro/IntroInfo.jsx
@@ -0,0 +1,57 @@
+import React, { useState } from "react";
+import { Tabs, TabList, TabPanels, Tab, TabPanel, Button, Flex, Center  } from "@chakra-ui/react"
+import styled from "styled-components";
+
+const Container = styled.div`
+  .tap {
+  width: 200px;
+  size: "md";
+  margin-left: 10px;
+  border: 1px solid teal;
+  border-radius: 6px;
+  
+  font-weight: bold;
+  color: teal;
+  }
+  .tabPanel{
+    height: 600px;
+  }
+`
+
+const IntroInfo = () => {
+
+  return ( 
+    <Container>
+      <Center>
+        <Tabs variant="unstyled" mt="30px">
+          <TabList>
+            <Tab className="tap" _selected={{ color: "white", bg: "teal" }}>곡 소개</Tab>
+            <Tab className="tap" _selected={{ color: "white", bg: "teal" }}>가사</Tab>
+            <Tab className="tap" _selected={{ color: "white", bg: "teal" }}>정보</Tab>
+            <Tab className="tap" _selected={{ color: "white", bg: "teal" }}>기타</Tab>
+            <Tab className="tap" _selected={{ color: "white", bg: "teal" }}>관련 미디어</Tab>
+          </TabList>
+          <TabPanels>
+            <TabPanel className="tabPanel">
+              <p>MOLTO의 의미/목적에 대해 쓰기</p>
+            </TabPanel>
+            <TabPanel className="tabPanel">
+              <p>MOLTO만의 특징? 이런 거 쓰기</p>
+            </TabPanel>
+            <TabPanel className="tabPanel">
+              <p>MOLTO 사용법</p>
+            </TabPanel>
+            <TabPanel className="tabPanel">
+              <p>푸터에 들어갈 내용</p>
+            </TabPanel>
+            <TabPanel className="tabPanel">
+              <p>시간되면 MOLTO 영상 제작하기</p>
+            </TabPanel>
+          </TabPanels>
+        </Tabs>
+      </Center>
+    </Container>
+  );
+};
+
+export default IntroInfo;
--- src/pages/MainPage/index.jsx
+++ src/pages/MainPage/index.jsx
@@ -1,27 +1,33 @@
 import React from "react";
-import Desc_slide from "../../components/MainPage/Desc_slide";
+import Header from "../../components/MainPage/menu/Header";
+import SideMenu from "../../components/MainPage/menu/SideMenu";
 import RecentSearch from "../../components/MainPage/RecentSearch";
-import Rank_Top100 from "../../components/MainPage/ranking/Rank_Top100";
-import Rank_Day from "../../components/MainPage/ranking/Rank_Day";
-import Rank_Week from "../../components/MainPage/ranking/Rank_Week";
-import Footer from '../../components/MainPage/Footer'
+import Footer from '../../components/MainPage/Footer';
+import HeadInfo from '../../components/MainPage/intro/HeadInfo';
+import IntroInfo from '../../components/MainPage/intro/IntroInfo';
+import styled from "styled-components";
 
-import { Container, Flex, Heading, Spacer } from "@chakra-ui/react";
+import { Flex, Heading, Spacer } from "@chakra-ui/react";
+
+const Container = styled.div`
+  z-index: 0;
+  margin: 0 auto;
+
+  width: 1200px;
+
+  box-shadow: 10px 4px 106px 12px rgba(0,0,0,0.1);
+  -webkit-box-shadow: 10px 4px 106px 12px rgba(0,0,0,0.1);
+  -moz-box-shadow: 10px 4px 106px 12px rgba(0,0,0,0.1);
+`
 
 const MainPage = () => {
   return (
     <>
-      <Desc_slide />
-      <Container maxW="1100px" mx="auto">  
-        <RecentSearch />
-        <Flex mt="100px">
-          <Heading fontSize="30px" color="gray.500" mr="30px">MUSIC<br/>RANKING</Heading>
-          <Rank_Top100 />
-          <Spacer /> 
-          <Rank_Day />
-          <Spacer />
-          <Rank_Week />
-        </Flex>
+      <Header />
+      <SideMenu />
+      <Container>
+          <HeadInfo />
+          <IntroInfo />   
       </Container>
       <Footer />
     </>
