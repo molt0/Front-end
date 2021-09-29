8d0159150d036cc32a915f03c0715b012c6e7cb3
jd053303 <jd053303@gmail.com>
2021-09-29 16:23:59 +0900

    fix: 디자인 수정

--- src/pages/LoginPage/index.jsx
+++ src/pages/LoginPage/index.jsx
@@ -5,8 +5,8 @@ import { Container, Flex, Box, Link, Text } from "@chakra-ui/react";
 const LoginPage = () => {
   return (
     <Container maxW="1500px" mx="auto">
-      <Flex mt="100px">
-        <Box width="500px" height="700px" maxW="1500px" mx="auto" mt="30px" borderRadius="md" boxShadow="lg" bg="#fbfbfb;">
+      <Flex mt="80px">
+        <Box width="500px" height="750px" maxW="1500px" mx="auto" mt="30px" borderRadius="md" boxShadow="lg" bg="#fbfbfb;">
           <LoginForm />
           <Text fontSize="sm" ml="110px" mt="100px">이곳이 처음이라면 <Link color="blue">여기</Link>를 눌러주세요.</Text>
         </Box>
--- src/pages/SignUpPage/index.jsx
+++ src/pages/SignUpPage/index.jsx
@@ -1,25 +1,14 @@
 import React from "react";
-import IdInput from "../../components/SignUpPage/IdInput";
-import PsChInput from "../../components/SignUpPage/PsChInput";
-import PsChInput2 from "../../components/SignUpPage/PsChInput2";
-import NameInput from "../../components/SignUpPage/NameInput";
-import EmailInput from "../../components/SignUpPage/EmailInput";
+import SignupForm from "../../components/SignUpPage/SignupForm";
 import { Container, Flex, Box, Heading, Button } from "@chakra-ui/react";
 
 const SignUpPage = () => {
   return (
     <Container maxW="1500px" mx="auto">
       <Flex mt="100px">
-        <Box width="600px" height="500px" maxW="1500px" mx="auto" mt="30px" borderRadius="md" boxShadow="lg" bg="#fbfbfb;">
-          <Heading width="150px" ml="180px" mt="50px">회원가입</Heading>
-          <IdInput />
-          <PsChInput />
-          <PsChInput2 />
-          <NameInput />
-          <EmailInput />
-          <Button variant="outline" width="250px" height="70px" left="120px" top="60px" borderRadius="lg" bg="teal.300" color="#fff" fontSize="xl" fontWeight="extrabold">
-            회원가입
-          </Button>
+        <Box width="600px" height="800px" maxW="1500px" mx="auto" borderRadius="md" boxShadow="lg" bg="#fbfbfb;">
+          <Heading width="150px" margin="0 auto" marginY="40px">회원가입</Heading>
+          <SignupForm />
         </Box>  
       </Flex>
     </Container>
