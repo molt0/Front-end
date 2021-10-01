d1f06b793e0a0bec8777a0d5c440adc60a5be466
jd053303 <jd053303@gmail.com>
2021-10-01 13:15:03 +0900

    feat: 최근 수정 리스트 레이아웃

---
+++ src/components/MainPage/ModifiedList.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+import styled from "styled-components";
+import { Box, Flex, Image, Text, Heading } from "@chakra-ui/react";
+
+const ModifiedList = () => {
+  return (
+    <div>
+      <Box bgColor="gray.500" width="200px" height="400px">
+          최근 수정 리스트
+      </Box>
+    </div>
+  );
+};
+
+export default ModifiedList;
