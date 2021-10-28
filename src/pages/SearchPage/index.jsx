9a1fb4f19e67411c122b00a819634f84f312bb4a
HARDTACK-Dev <rjsdn827@gmail.com>
2021-10-29 02:17:37 +0900

    feat: 검색페이지 기능 / 디자인 구현

--- src/pages/SearchPage/index.jsx
+++ src/pages/SearchPage/index.jsx
@@ -6,13 +6,41 @@ import {
   RadioGroup,
   Select, 
   SimpleGrid, 
-  Stack
+  Stack,
+  Input
 } from '@chakra-ui/react'
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import AlbumList from '../../components/SearchPage/AlbumList'
+import styled, { css } from "styled-components";
+import Api from '../../Api'
+
+const NoData = styled.div`
+  display: ${props => props.noData ? 'block' : 'none'};
+  width: 100%;
+  height: 100px;
+
+  & p{
+    font-size: 50px;
+    text-align: center;
+
+    margin-top: 290px;
+  }
+`;
+
+const NoResult = styled.h1`
+   display: ${props => props.visible ? 'block' : 'none'};
+    font-size: 30px;
+
+    text-align: center;
+
+    color: red;
+`;
 
 const SearchPage = () =>{
   const [isVertHoriz, setIsVertHoriz] = useState("horizontal")
+  const [isNoData, setNoData] = useState(false)
+  const [searchResultIsNull, setSearchResultIsNull] = useState(false)
+  const [query, setQuery] = useState("")
   function VH_more(){
     if(isVertHoriz ==  "horizontal") 
       setIsVertHoriz("vertical") 
@@ -22,38 +50,22 @@ const SearchPage = () =>{
 
   // 긁어온 음원 목록 (원본)
   const [moltoList, setmoltoList] = useState([
-    {img:"음원_사진", artist:"아티스트_이름", name:"노래_제목1", time:200, like:4431, date: new Date("2021-09-05"), type:"발라드"},
-    {img:"음원_사진", artist:"아티스트_이름", name:"노래_제목2", time:200, like:322, date: new Date("2021-09-01"), type:"팝"},
-    {img:"음원_사진", artist:"아티스트_이름", name:"노래_제목3", time:200, like:333, date: new Date("2021-09-05"), type:"락"},
-    {img:"음원_사진", artist:"아티스트_이름", name:"노래_제목4", time:200, like:241, date: new Date("2021-09-03"), type:"힙합"},
-    {img:"음원_사진", artist:"아티스트_이름", name:"노래_제목5", time:200, like:5546, date: new Date("2021-09-03"), type:"발라드"},
-    {img:"음원_사진", artist:"아티스트_이름", name:"노래_제목6", time:200, like:6432, date: new Date("2021-09-04"), type:"힙합"},
-    {img:"음원_사진", artist:"아티스트_이름", name:"노래_제목7", time:200, like:4327, date: new Date("2021-09-03"), type:"팝"},
-    {img:"음원_사진", artist:"아티스트_이름", name:"노래_제목8", time:200, like:844, date: new Date("2021-09-04"), type:"팝"},
-    {img:"음원_사진", artist:"아티스트_이름", name:"노래_제목9", time:200, like:9131, date: new Date("2021-09-03"), type:"락"},
-    {img:"음원_사진", artist:"아티스트_이름", name:"노래_제목10", time:200, like:1220, date: new Date("2021-09-03"), type:"락"},
-    {img:"음원_사진", artist:"아티스트_이름", name:"노래_제목11", time:200, like:1333, date: new Date("2021-09-04"), type:"힙합"},
-    {img:"음원_사진", artist:"아티스트_이름", name:"노래_제목12", time:200, like:1132, date: new Date("2021-09-03"), type:"발라드"},
-    {img:"음원_사진", artist:"아티스트_이름", name:"노래_제목13", time:200, like:113, date: new Date("2021-09-02"), type:"팝"},
-    {img:"음원_사진", artist:"아티스트_이름", name:"노래_제목14", time:200, like:1321, date: new Date("2021-09-03"), type:"발라드"},
-    {img:"음원_사진", artist:"아티스트_이름", name:"노래_제목15", time:200, like:3211, date: new Date("2021-09-03"), type:"팝"},
-    {img:"음원_사진", artist:"아티스트_이름", name:"노래_제목16", time:200, like:32, date: new Date("2021-09-01"), type:"락"},
-    {img:"음원_사진", artist:"아티스트_이름", name:"노래_제목17", time:200, like:4431, date: new Date("2021-09-05"), type:"발라드"},
-    {img:"음원_사진", artist:"아티스트_이름", name:"노래_제목18", time:200, like:322, date: new Date("2021-09-01"), type:"팝"},
-    {img:"음원_사진", artist:"아티스트_이름", name:"노래_제목19", time:200, like:333, date: new Date("2021-09-05"), type:"락"},
-    {img:"음원_사진", artist:"아티스트_이름", name:"노래_제목20", time:200, like:241, date: new Date("2021-09-03"), type:"힙합"},
-    {img:"음원_사진", artist:"아티스트_이름", name:"노래_제목21", time:200, like:5546, date: new Date("2021-09-03"), type:"발라드"},
-    {img:"음원_사진", artist:"아티스트_이름", name:"노래_제목22", time:200, like:6432, date: new Date("2021-09-04"), type:"힙합"},
-    {img:"음원_사진", artist:"아티스트_이름", name:"노래_제목23", time:200, like:4327, date: new Date("2021-09-03"), type:"팝"},
-    {img:"음원_사진", artist:"아티스트_이름", name:"노래_제목23", time:200, like:844, date: new Date("2021-09-04"), type:"팝"},
-    {img:"음원_사진", artist:"아티스트_이름", name:"노래_제목24", time:200, like:9131, date: new Date("2021-09-03"), type:"락"},
-    {img:"음원_사진", artist:"아티스트_이름", name:"노래_제목25", time:200, like:1220, date: new Date("2021-09-03"), type:"락"},
-    {img:"음원_사진", artist:"아티스트_이름", name:"노래_제목26", time:200, like:1333, date: new Date("2021-09-04"), type:"힙합"},
-    {img:"음원_사진", artist:"아티스트_이름", name:"노래_제목27", time:200, like:1132, date: new Date("2021-09-03"), type:"발라드"},
-    {img:"음원_사진", artist:"아티스트_이름", name:"노래_제목28", time:200, like:113, date: new Date("2021-09-02"), type:"팝"},
-    {img:"음원_사진", artist:"아티스트_이름", name:"노래_제목29", time:200, like:1321, date: new Date("2021-09-03"), type:"발라드"},
-    {img:"음원_사진", artist:"아티스트_이름", name:"노래_제목30", time:200, like:3211, date: new Date("2021-09-03"), type:"팝"}
+    // {img:"음원_사진", artist:"아티스트_이름", name:"노래_제목30", time:200, like:3211, date: new Date("2021-09-03"), type:"팝"}
+    // 객체가 
   ])
+
+   // 검색 데이터 존재 여부 검사
+   useEffect(()=>{
+    if(moltoList.length === 0){
+      setNoData(true)
+    }else{
+      setNoData(false)
+    }
+    // 검색창이 비어있는지 검사
+    if(query == "")
+      setSearchResultIsNull(false)
+    
+  }, [moltoList, query])
 
   // 순서 정렬
   const [isContentSort, setIsContentSort] = useState("")
@@ -61,11 +73,25 @@ const SearchPage = () =>{
   // 장르 필터
   const [genre, setGenre] = useState("")
 
+  async function getData(){
+    console.log(query)
+    setmoltoList([])
+
+    Api.get(`/search/${query}`).then((res)=>{
+      console.log(res.data)
+
+      if(res.data.result === false)
+        setSearchResultIsNull(true)
+      else
+        setmoltoList(res.data.result)
+    })
+  }
+
 
   return (
     <Box >
       <Flex h={50} lineHeight="50px" bgColor="#70ebe0">
-        <Box flex="2" ml={5}>
+        <Box ml={5}>
           <Select w={200} placeholder="모든 장르" onChange={(e)=>{setGenre(e.target.value) }}>
             <option value="발라드">발라드</option>
             <option value="팝">팝</option>
@@ -74,6 +100,12 @@ const SearchPage = () =>{
           </Select>
         </Box>
 
+        <Input variant="filled" placeholder="Filled" width="700px" mt="5px" ml="10vw" 
+        onChange={(e)=>{setQuery(e.target.value)}} />
+        <Button colorScheme="teal" size="md" ml="10px" mt="5px" onClick={getData}>
+          검색
+        </Button>
+
         <Flex flex="1" justifyContent="space-around">
           <RadioGroup w="300px" onChange={(value)=>{setIsContentSort(value)}}>
             <Stack direction="row">
@@ -81,12 +113,17 @@ const SearchPage = () =>{
               <Radio value="인기" >인기순</Radio>
             </Stack>
           </RadioGroup>
-          <Stack direction="row">
+          <Stack direction="row" width="90px">
             <Box>세로</Box>
             <Switch w={50} h={50} lineHeight="60px" size="lg" onChange={VH_more}/>
           </Stack>
         </Flex>
       </Flex>
+
+      <NoData noData={isNoData}>
+        <p>검색창에 노래제목을 검색해보세요!</p>
+        <NoResult visible={searchResultIsNull && isNoData == true}>{query}와 관련된 문서가 없어요..!</NoResult>
+      </NoData>
 
       <SimpleGrid w="1000px" m="0 auto " p="10px" spacing="20px" columns={isVertHoriz == "horizontal" ? "5" : "1"}> 
         <AlbumList albums={moltoList} isVertHoriz={isVertHoriz} isContentSort={isContentSort} genre={genre}/>
